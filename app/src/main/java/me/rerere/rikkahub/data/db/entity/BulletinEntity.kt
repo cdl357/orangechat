39c60279ab41ef7f8280dfc693870fb23846599d
cdl357 <3104917311@qq.com>
2026-08-27 08:42:36 +0800

    留言板上云：BulletinEntity 加 remoteId + 索引

--- app/src/main/java/me/rerere/rikkahub/data/db/entity/BulletinEntity.kt
+++ app/src/main/java/me/rerere/rikkahub/data/db/entity/BulletinEntity.kt
@@ -7,13 +7,17 @@ package me.rerere.rikkahub.data.db.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 /**
  * 留言板条目
  * author: "sean" | "yuri"
  */
-@Entity(tableName = "bulletin_note")
+@Entity(
+    tableName = "bulletin_note",
+    indices = [Index(value = ["remote_id"])],
+)
 data class BulletinEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
@@ -36,6 +40,16 @@ data class BulletinEntity(
      */
     @ColumnInfo("reply_to")
     val replyTo: Int = 0,
+
+    /**
+     * 云端 `bulletin_notes` 表那一行的 id。
+     *
+     * 服务器上的 daemon 独处时会往云端写便签，App 拉下来时靠这个字段去重：
+     * remote_id 已经存在就跳过，反复同步不会插成好几份。
+     * 空串 = 这张便签目前只在本地。
+     */
+    @ColumnInfo("remote_id", defaultValue = "")
+    val remoteId: String = "",
 
     /** 创建时间戳 */
     @ColumnInfo("created_at")
